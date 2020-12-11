75c075a3a9487dbfa6346bed7ad41a1bdeecaf1f
Xu Yuandong <786018072@qq.com>
2020-12-11 16:16:50 +0800

    init vue-3 & rebase

--- src/main.js
+++ src/main.js
@@ -1,4 +1,44 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+//import Vuex from "vuex";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./libs/router";
+import { Layout,
+         Menu,
+         Button,
+         Row,
+         Divider,
+         Col } from "ant-design-vue";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+//import modules from "./store/index";
 
-createApp(App).mount('#app')
+NProgress.configure({
+  easing: "ease",
+  speed: 500,
+  showSpinner: false,
+  trickleSpeed: 200,
+  minimum: 0.3
+});
+//当路由进入前
+router.beforeEach((to, from, next) => {
+  // 每次切换页面时，调用进度条
+  NProgress.start();
+  NProgress.inc();
+  next();
+});
+//当路由进入后：关闭进度条
+router.afterEach(() => {
+  NProgress.done();
+});
+
+//const store = new Vuex.Store({ modules });
+createApp(App)
+  .use(router)
+  .use(Button)
+  .use(Layout)
+  .use(Menu)
+  .use(Row)
+  .use(Col)
+  .use(Divider)
+  .mount("#app");
