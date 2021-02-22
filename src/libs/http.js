d79162c79aa172d3d0ed6fe16a819a94132a91bb
yuandongx <786018072@qq.com>
2021-02-22 10:34:21 +0800

    【U】update

--- src/libs/http.js
+++ src/libs/http.js
@@ -5,10 +5,5 @@ axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.delete['Content-Type'] = 'application/json';
 axios.defaults.headers.patch['Content-Type'] = 'application/json';
 axios.defaults.baseURL = "http://127.0.0.1:8888";
-axios.interceptors.request.use(config=>{
-    return config;
-}, error => {
-    return Promise.reject(error);
-});
 
 export default axios;
